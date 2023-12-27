ddd48b7e3b2503782da956f5fea26024d3482baf
Dmitriy <limbo.crow@mail.ru>
2023-12-28 01:00:03 +0300

    Trying front-end

--- zavodsocialnetwork.client/src/App.tsx
+++ zavodsocialnetwork.client/src/App.tsx
@@ -1,7 +1,8 @@
-import './Header.tsx'
-import './App.css';
-import Header from "./Header.tsx";
-
+import Header from "./components/Header.tsx";
+let rows : Array<string> = [];
+for(let i : number = 0; i < 50; i++){
+    rows.push("<h1>Another shit use of react in C#</h1>");
+}
 export default function App() {
     /*useEffect(() => {
         populateWeatherData();
@@ -11,18 +12,7 @@ export default function App() {
         <div>
             <h1 id="tabelLabel">TESTING___TESTING</h1>
             <p>This component demonstrates fetching data from the server.</p>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
-            <h1>Another shit use of react in C#</h1>
+            <h1>{rows}</h1>
         </div>
         </>
     );
---
+++ zavodsocialnetwork.client/src/components/Header.tsx
@@ -0,0 +1,26 @@
+﻿//import { useEffect, useState } from 'react';
+import '../scss/Header.scss';
+import './RegLoginButton.tsx'
+import RegLoginButton from "./RegLoginButton.tsx";
+
+export default function Header(): JSX.Element {
+    /*
+    Страничка своя
+    Уведмоления/сообщения
+    Войти/регистер
+    ЭТО ВСЁ В БОКОВУЮ ШТУКУ
+    Сверху какой-нибудь контент?
+     */
+    
+    return <header>
+            <h1>Zavodchanins social network kinda</h1>
+        <div className={"button_container"}>
+            <a className={"header_button"}>Your profile</a>
+            <a className={"header_button"}>Messages</a>
+            <a className={"header_button"}>Search engine</a>
+        </div>
+            <RegLoginButton></RegLoginButton>
+        
+        </header>
+}
+
