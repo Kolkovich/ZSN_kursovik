cd548e53b8d7bcbbb251ed53e65f5482db5f6497
Qemar <beloedrevo@mail.ru>
2023-12-28 07:00:05 +0300

    BackPain

--- ZavodSocialNetwork.Server/Program.cs
+++ ZavodSocialNetwork.Server/Program.cs
@@ -1,3 +1,7 @@
+using System.Net.Mime;
+using Microsoft.EntityFrameworkCore;
+using ZavodSocialNetwork.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,11 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
+builder.Services.AddScoped(typeof(DbContext), typeof(DbContext));
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
