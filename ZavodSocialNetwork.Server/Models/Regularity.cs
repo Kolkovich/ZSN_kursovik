6770c46532cd36b9a02ecf99a606229e480d3945
Qemar <beloedrevo@mail.ru>
2023-12-28 07:04:27 +0300

    BackPain Acktual:(

---
+++ ZavodSocialNetwork.Server/Models/Regularity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ZavodSocialNetwork.Server.Models;
+
+public class Regularity
+{
+    [Required]
+    public int Id { get; set; }
+    
+    public string How_many { get; set; }
+    public string How_often { get; set; }
+}
---
+++ ZavodSocialNetwork.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ZavodSocialNetwork.Server.Models;
+
+public class User
+{
+    [Required]
+    public int Id { get; set; }
+    
+    public string Role { get; set; }
+    public string Phone { get; set; }
+    public string Organization { get; set; }
+}
